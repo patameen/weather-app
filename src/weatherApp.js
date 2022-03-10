603e3b050fdf6cc2b78d3c5b6a990aa354f124ad
patameen <patricija.vendina@gmail.com>
2022-03-10 11:11:46 +0200

    arranged the js in proper order, add  wind, humid

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -19,76 +19,84 @@ formatDate(new Date());
 //Search city, show in h1 and temperature in h2
 //Note to self:Still need .trim() in case of searching spaces!!!
 
-function geolocal(event) {
-  event.preventDefault();
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let key = "7dfa514196d8ad8df497144d715c56b0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+  console.log(apiUrl);
+  console.log(latitude);
+  console.log(longitude);
+  function showTemp(response) {
+    let temperature = Math.round(response.data.main.temp);
+    console.log(temperature);
+    let locationCity = response.data.name;
+    console.log(locationCity);
+    let weatherDescription = response.data.weather[0].description;
+    console.log(weatherDescription);
+    let humidity = response.data.main.humidity;
+    let wind = response.data.wind.speed;
 
-  function showPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let key = "7dfa514196d8ad8df497144d715c56b0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
-    console.log(apiUrl);
-    console.log(latitude);
-    console.log(longitude);
-    function showTemp(response) {
-      let temperature = Math.round(response.data.main.temp);
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = `${locationCity}`;
+    let h2 = document.querySelector("h2");
+    h2.innerHTML = `${temperature}`;
+    let description = document.querySelector("#weather-description");
+    description.innerHTML = `${weatherDescription}`;
+    let humidityInfo = document.querySelector("#humidity");
+    humidityInfo.innerHTML = `${humidity}`;
+    let windInfo = document.querySelector("#wind");
+    windInfo.innerHTML = `${Math.round(wind)}`;
+
+    function changeToFah(event) {
+      event.preventDefault();
+      let fahrenheitButton = document.querySelector("#fahrenheitButton");
+      let celsiusButton = document.querySelector("#celsiusButton");
+      h2.innerHTML = `${temperature * 1.8 + 32}`;
+      fahrenheitButton.innerHTML = `<button id="fahrenheitButton"><strong>F</strong></button>`;
+      celsiusButton.innerHTML = `<button id="celsiusButton">C /</button>`;
+    }
+    function changeToCel(event) {
+      event.preventDefault();
+      let fahrenheitButton = document.querySelector("#fahrenheitButton");
+      let celsiusButton = document.querySelector("#celsiusButton");
+      h2.innerHTML = `${temperature}`;
       console.log(temperature);
-      let locationCity = response.data.name;
-      console.log(locationCity);
-      let weatherDescription = response.data.weather[0].description;
-      console.log(weatherDescription);
-      let h1 = document.querySelector("h1");
-      h1.innerHTML = `${locationCity}`;
+      fahrenheitButton.innerHTML = `<button id="fahrenheitButton">F</button>`;
+      celsiusButton.innerHTML = `<button id="celsiusButton"><strong>C</storng> /</button>`;
+    }
+    let tempC = document.querySelector("#celsiusButton");
+    tempC.addEventListener("click", changeToCel);
+    let tempF = document.querySelector("#fahrenheitButton");
+    tempF.addEventListener("click", changeToFah);
+  }
+
+  function changeCity(event) {
+    event.preventDefault();
+    let citySearch = document.querySelector("#enter-city");
+    let cityName = document.querySelector("h1");
+    cityName.innerHTML = `${citySearch.value}`;
+
+    let apiKey = "7dfa514196d8ad8df497144d715c56b0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&units=metric&appid=${apiKey}`;
+
+    function showWeather(response) {
+      let temperature = Math.round(response.data.main.temp);
       let h2 = document.querySelector("h2");
       h2.innerHTML = `${temperature}`;
-      let description = document.querySelector("#weather-description");
-      description.innerHTML = `${weatherDescription}`;
-
-      function changeToFah(event) {
-        event.preventDefault();
-        let fahrenheitButton = document.querySelector("#fahrenheitButton");
-        let celsiusButton = document.querySelector("#celsiusButton");
-        h2.innerHTML = `${temperature * 1.8 + 32}`;
-        fahrenheitButton.innerHTML = `<button id="fahrenheitButton"><strong>F</strong></button>`;
-        celsiusButton.innerHTML = `<button id="celsiusButton">C /</button>`;
-      }
-      function changeToCel(event) {
-        event.preventDefault();
-        let fahrenheitButton = document.querySelector("#fahrenheitButton");
-        let celsiusButton = document.querySelector("#celsiusButton");
-        h2.innerHTML = `${temperature}`;
-        console.log(temperature);
-        fahrenheitButton.innerHTML = `<button id="fahrenheitButton">F</button>`;
-        celsiusButton.innerHTML = `<button id="celsiusButton"><strong>C</storng> /</button>`;
-      }
-      let tempC = document.querySelector("#celsiusButton");
-      tempC.addEventListener("click", changeToCel);
-      let tempF = document.querySelector("#fahrenheitButton");
-      tempF.addEventListener("click", changeToFah);
     }
-
-    function changeCity(event) {
-      event.preventDefault();
-      let citySearch = document.querySelector("#enter-city");
-      let cityName = document.querySelector("h1");
-      cityName.innerHTML = `${citySearch.value}`;
-
-      let apiKey = "7dfa514196d8ad8df497144d715c56b0";
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&units=metric&appid=${apiKey}`;
-
-      function showWeather(response) {
-        let temperature = Math.round(response.data.main.temp);
-        let h2 = document.querySelector("h2");
-        h2.innerHTML = `${temperature}`;
-      }
-      axios.get(apiUrl).then(showWeather);
-    }
-    let cityInfo = document.querySelector("#city-form");
-    cityInfo.addEventListener("submit", changeCity);
-    //Geolocation after pressing arrow
-
-    axios.get(apiUrl).then(showTemp);
+    axios.get(apiUrl).then(showWeather);
   }
+  let cityInfo = document.querySelector("#city-form");
+  cityInfo.addEventListener("submit", changeCity);
+  //Geolocation after pressing arrow
+
+  axios.get(apiUrl).then(showTemp);
+}
+navigator.geolocation.getCurrentPosition(showPosition);
+
+function geolocal(event) {
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 let locationButton = document.querySelector("#location-now");
